8acf6190b43b57d0f477f14f1b92e1b6761e7968
Credventloans05 <credventloans.doc5@gmail.com>
2026-06-16 10:06:31 +0530

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+/*
+  Supabase configuration file.
+  Replace the placeholder values below with your own Supabase project details.
+  Do NOT commit your actual SUPABASE_ANON_KEY or SUPABASE_URL to a public repository.
+*/
+window.SUPABASE_URL = 'https://uuihsmyzdzrxtpgmsbkv.supabase.co';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1aWhzbXl6ZHpyeHRwZ21zYmt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5OTcyMzgsImV4cCI6MjA5NjU3MzIzOH0.0nyMF6HhpXwqTyWi1gqavQK14K7KvjYky7h9YvKlvLY';
